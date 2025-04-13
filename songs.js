c68d1a3c370c68e9de6ddd6d8b7937965a69092d
Akshatsharma234 <akshatsharma21398@gmail.com>
2025-04-13 23:46:33 +0530

    Add files via upload

---
+++ songs.js
@@ -0,0 +1,33 @@
+// Sample songs data
+const songs = [
+    {
+        title: "Dhanda Nyoliwala",
+        artist: "Knife Brows",
+        src: "Songs/Knife Brows - Dhanda Nyoliwala.mp3" , // Placeholder audio
+        cover: "Images/Knife blows.jpg"
+    },
+    {
+        title: "Follow You",
+        artist: "Imagine Dragons",
+        src: "Songs/Follow you.mp3", // Placeholder audio
+        cover: "Images/Follow you.jpg"
+    },
+    {
+        title: "I wanna be your slave",
+        artist: "Maneskin",
+        src: "Songs/Mneskin_-_I_WANNA_BE_YOUR_SLAVE_72864238.mp3", // Placeholder audio
+        cover: "Images/MANESKIN.jpg"
+    },
+    {
+        title: "Wavy",
+        artist: "Karan Aujla",
+        src: "Songs/Wavy.mp3", // Placeholder audio
+        cover: "Images/Wavy.jpg"
+        },
+        {
+            title: "Russian Bandana",
+            artist: "Dhanda Nyoliwala",
+            src: "Songs/Russian Bandana.mp3", // Placeholder audio
+            cover: "Images/Russian Bandana.jpg"
+        },
+    ];
